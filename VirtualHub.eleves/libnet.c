6d7fc83477bd39df2f3dbd7dedbbc8edb001d6da
Smagghe Cyril <cyrilsmg@users.noreply.github.com>
2015-02-24 10:22:22 +0100

    Update libnet.c

--- VirtualHub.eleves/libnet.c
+++ VirtualHub.eleves/libnet.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-
+#include <poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
@@ -68,16 +68,61 @@ if(statut<0) return -1;
 return s;
 }
 
-int boucleServeur(int ecoute,int (*traitement)(int))
+int boucleServeur(SOCKET sock,int (*traitement)(int))
 {
-//int dialogue;
-while(1){
-    /* Attente d'une connexion */
-    if((dialogue=accept(ecoute,NULL,NULL))<0) return -1;
-    /* Passage de la socket de dialogue a la fonction de traitement */
-    if(traitement(dialogue)<0){ close(ecoute); return 0;}
-    }
- 
+struct pollfd descripteurs[MAX_CONNEXION+1];
+int i;
+int c=0;
+for(i=0; i<MAX_CONNEXION; i++)
+	{
+	descripteurs[i].fd = SOCKET_ERROR;
+	descripteurs[i].events = POLLIN;
+	}
+descripteurs[MAX_CONNEXION].fd = sock;
+descripteurs[MAX_CONNEXION].events = POLLIN;
+while(1)
+	{
+	printf("on attend que ca arrive\n");
+	int nb = poll(descripteurs, MAX_CONNEXION+1, -1);
+	if(nb<0)
+		{
+		printf("poll failed\n");
+		exit(EXIT_FAILURE);
+		}
+	for(i=0; i<MAX_CONNEXION; i++)
+		{
+		if((descripteurs[i].revents&POLLIN)!=0)
+			{
+			printf("un client me parle\n");
+			if(traitement(descripteurs[i].fd) <= 0)
+				{
+				printf("oh ! :( un client est parti\n");
+				close(descripteurs[i].fd);
+				descripteurs[i].fd = SOCKET_ERROR;
+				}
+			}
+		}
+		if((descripteurs[MAX_CONNEXION].revents&POLLIN)!=0)
+			{
+			printf("ouais ! Un nouveau client :D !\n");
+			c=0;
+			while(c<MAX_CONNEXION && (descripteurs[c].fd != SOCKET_ERROR))
+			c++;
+			if(c==MAX_CONNEXION)
+				printf("Desole on est complet ce soir\n");
+			else
+				{
+				if((descripteurs[c].fd = accept(descripteurs[MAX_CONNEXION].fd, NULL, NULL)) < 0)
+				return -1;
+				if(traitement(descripteurs[c].fd) <= 0)
+					{
+					printf("Un client est parti :'(\n");
+					close(descripteurs[c].fd);
+					descripteurs[c].fd = SOCKET_ERROR;
+					}
+				}
+			}
+		}
 }
 
 
@@ -101,6 +146,15 @@ freeaddrinfo(resultat);
 return s;
 }
 
+int ecouterClient(SOCKET sock)
+{
+	char buffer[BUF_SIZE];
+	int n = 0;
+	n = recv(sock, buffer, BUF_SIZE - 1, 0);
+	buffer[n] = 0;
+	printf("Message recu 5 sur 5 : %s", buffer);
+	return n;
+}
 
 /**** Fonctions de gestion des interfaces virtuelles ****/
 
