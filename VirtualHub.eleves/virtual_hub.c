4c8ffdd56e07ec36023469458c5fbc92bf02dda2
Smagghe Cyril <cyrilsmg@users.noreply.github.com>
2015-02-24 10:27:11 +0100

    Update virtual_hub.c

--- VirtualHub.eleves/virtual_hub.c
+++ VirtualHub.eleves/virtual_hub.c
@@ -2,11 +2,22 @@
 
 /** Fichiers d'inclusion **/
 
-#include <stdio.h>
-#include <stdlib.h>
 
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
 
 #include "libnet.h"
 
@@ -36,8 +47,7 @@ return 0;
 /* Fonction principale */
 int main(int argc,char *argv[]){
 
-for(int i=0;i<MAX_CONNEXION;i++) tabport[i]=-1;
-
+SOCKET sock;
 // Analyse des arguments
 if(argc!=2){
   fprintf(stderr,"Syntaxe : switch <port>\n");
@@ -48,53 +58,14 @@ short int port=atoi(argv[1]);
 fprintf(stdout,"Port : %d\n",port);
 #endif
 
-// Initialisation du serveur
-    //int ts=initialisationServeur("4000",10);
-// Traitement des connexions et des messages
-    //boucleServeur(ts,gestionClient);
-    
-struct pollfd descripteurs[MAX_CONNEXION];
-int compteur;
-descripteurs[0].fd=s;
-descripteurs[0].events=POLLIN;
-
-while(1){
-    compteur=1;
-    char tampon[MAX_TAMPON];
-    for(int k=0;k<MAX_CONNEXION;k++){
-        if(tabPort[k].fd!=(-1)) {
-            descripteurs[compteur].fd=tabport[k].fd;
-            descripteurs[compteur].events=POLLIN;
-            compteur++;
-        }
-    }
-    int nb=poll(descripteurs,compteur,-1);
-    if(nb<0){ perror("main.poll"); exit(EXIT_FAILURE); }
-    if((descripteurs[0].revents&POLLIN)!=0){
-      i=0;
-      while(i<MAX_CONNEXIONS && (descripteurs[i].fd != SOCKET_ERROR))
-      i++;
-      if(i==MAX_CONNEXIONS)
-      else{
-        if((desc[i].fd = accept(descripteurs[MAX_CONNEXIONS].fd, NULL, NULL)) < 0)
-          return -1;
-        if(clientProcess(descripteurs[i].fd) <= 0){
-          close(descripteurs[i].fd);
-          descripteurs[i].fd = SOCKET_ERROR;
-          }
-      }      
-    }
-    for(int k=1;k<compteur;k++){
-        if((descripteurs[k].revents&POLLIN)!=0){
-          int taille=read(0,tampon,MAX_TAMPON);
-          if(taille<=0) {
-            for(int l=0;l<MAX_CONNEXION;l++){
-                if(descripteurs[k].fd = tabport[l].fd) tabport[l].fd=-1;
-            }
-          }
-          else write(s,tampon,taille);
-        }
-    }
-} 
+//Initialisation du serveur
+if((sock = initialisationServeur(port)) == SOCKET_ERROR)
+{
+printf("L initialisation a foire\n");
+return 1;
+}
+//serverLoop(sock, clientManagement);
+boucleServeur(sock, ecouterClient);
 return 0;
+
 }
